cfb932f69b5b5d21b0573d705a48d2aaa41ada3c
amandelman <adam.mandelman@gmail.com>
2020-03-26 18:00:44 +0100

    Some rough mixins & a JS refactor

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,7 @@
 const mobileMenu = document.querySelector(".menu");
 const burgerMenu = document.querySelector(".burger");
 
-function toggleBurger(){
-    mobileMenu.classList.toggle("menu-collapsed");
-}
-
-burgerMenu.addEventListener("click", toggleBurger);
+burgerMenu.onclick = function(){mobileMenu.classList.toggle("menu-collapsed")};
 
 
 
